c38e7ec8de319d70b9c9c05d8c4e333e51ebf0fb
Giant-Diagram <guentenspergerg@bzz.ch>
2023-06-09 09:07:35 +0200

    updated website

--- scs_supercarps/src/App.js
+++ scs_supercarps/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage.js';
+import HomePage from './pages/homePage.js';
 import Cars from './pages/cars.js';
+import About from './pages/about.js';
+import Contact from './pages/contact.js';
 
 function App() {
   return (
@@ -13,8 +15,8 @@ function App() {
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/cars">Cars</Link></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to ="contact">Contact</Link></li>
             </ul>
           </nav>
         </header>
@@ -23,6 +25,8 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/cars" element={<Cars />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
 
---
+++ scs_supercarps/src/pages/about.js
@@ -0,0 +1,23 @@
+// About.js
+import React from 'react';
+import './about.css';
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse viverra ligula vel nisi
+        ullamcorper, et pulvinar mi tincidunt. Aliquam ullamcorper id mauris ut fringilla. Nullam nec
+        mauris commodo, scelerisque enim id, auctor mi. Nunc mattis interdum mi, ac aliquet nunc auctor
+        ac. Nulla volutpat pharetra ultrices. Proin tincidunt, risus eget suscipit fermentum, metus
+        urna lobortis sem, vel luctus orci neque ac tortor. In ac justo eget eros iaculis ultricies ac
+        a elit. Aliquam a rhoncus tellus. Sed rutrum consequat mauris, et eleifend est fermentum at.
+        Suspendisse potenti. Integer auctor, orci id posuere laoreet, metus purus dignissim sapien, in
+        ullamcorper turpis est nec lacus.
+      </p>
+    </div>
+  );
+}
+
+export default About;
